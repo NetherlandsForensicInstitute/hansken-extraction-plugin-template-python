b7b3d09a5357af6e8f5ede42484639ab027fad40
Mahmoud Abdelghany <blackbeard334@users.noreply.github.com>
2021-12-03 14:26:16 +0100

    updated to newer 'minimal' version plugin_info

--- plugin/extraction_plugin.py
+++ plugin/extraction_plugin.py
@@ -1,5 +1,5 @@
 from hansken_extraction_plugin.api.extraction_plugin import ExtractionPlugin
-from hansken_extraction_plugin.api.plugin_info import Author, MaturityLevel, PluginInfo
+from hansken_extraction_plugin.api.plugin_info import Author, MaturityLevel, PluginId, PluginInfo
 from logbook import Logger
 
 log = Logger(__name__)
@@ -10,13 +10,14 @@ class Plugin(ExtractionPlugin):
     def plugin_info(self):
         plugin_info = PluginInfo(
             self,
-            name='your_plugin_name',
             version='0.0.0',
             description='description of your plugin',
             author=Author('Your name', 'your@email.address', 'your organisation'),
             maturity=MaturityLevel.PROOF_OF_CONCEPT,
             webpage_url='',  # e.g. url to the code repository of your plugin
-            matcher='* and $data.type=raw'
+            matcher='$data.type=raw',
+            id=PluginId('domain', 'category', 'your_plugin_name'),
+            license='Apache License 2.0'
         )
         return plugin_info
 
